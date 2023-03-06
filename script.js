ac63fb29f590c75e78676977e4af08a7a60fe064
Prince <prius2055@gmail.com>
2023-03-06 19:50:18 +0100

    Modify script file

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const mobileNav = document.querySelector(".mobile-nav");
-const hamburger = document.querySelector(".hamburger");
-const mobileNavLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("active");
-  mobileNav.classList.toggle("active");
-});
-
-mobileNavLink.forEach((nav) => {
-  nav.addEventListener("click", function () {
-    mobileNav.classList.remove("active");
-    hamburger.classList.remove("active");
-  });
-});
 
 
