4b7b4f26eaf1fe1457b4d165563db74b0942804c
Prince <prius2055@gmail.com>
2023-03-06 17:01:18 +0100

    Modify index.html file,styles.css file and script.css file

---
+++ script.js
@@ -0,0 +1,17 @@
+const mobileNav = document.querySelector(".mobile-nav");
+const hamburger = document.querySelector(".hamburger");
+const mobileNavLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+});
+
+mobileNavLink.forEach((nav) => {
+  nav.addEventListener("click", function () {
+    mobileNav.classList.remove("active");
+    hamburger.classList.remove("active");
+  });
+});
+
+console.log(mobileNavLink);
