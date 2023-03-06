35b329bb2dd211b3a517e39237825068b242c3e5
Prince <prius2055@gmail.com>
2023-03-07 00:18:23 +0100

    Made corrections to linters

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
+const mobileNav = document.querySelector('.mobile-nav');
+const hamburger = document.querySelector('.hamburger');
+const mobileNavLink = document.querySelectorAll('.nav-link');
 
-const mobileNav = document.querySelector(".mobile-nav");
-const hamburger = document.querySelector(".hamburger");
-const mobileNavLink = document.querySelectorAll(".nav-link");
-
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("active");
-  mobileNav.classList.toggle("active");
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('active');
+  mobileNav.classList.toggle('active');
 });
 
 mobileNavLink.forEach((nav) => {
-  nav.addEventListener("click", function () {
-    mobileNav.classList.remove("active");
-    hamburger.classList.remove("active");
+  nav.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+    hamburger.classList.remove('active');
   });
 });
