a5282747e4c0cda504476b52c2fbb83da2d5d7d7
Prince <prius2055@gmail.com>
2023-03-08 13:00:57 +0100

    Modify css & js file:Adjustment made following linters recommendations

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const projects = [
     name: 'Professional Art Printing data',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: ``,
+    image: '',
     technology: ['html', 'Bootstrap', 'Ruby on rails'],
     liveVersion: '',
     source: '',
@@ -13,7 +13,7 @@ const projects = [
     name: 'Professional Art Printing data',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: ``,
+    image: '',
     technology: ['html', 'Bootstrap', 'Ruby on rails'],
     liveVersion: '',
     source: '',
@@ -23,7 +23,7 @@ const projects = [
     name: 'Professional Art Printing data',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: ``,
+    image: '',
     technology: ['html', 'Bootstrap', 'Ruby on rails'],
     liveVersion: '',
     source: '',
@@ -33,7 +33,7 @@ const projects = [
     name: 'Professional Art Printing data',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: ``,
+    image: '',
     technology: ['html', 'Bootstrap', 'Ruby on rails'],
     liveVersion: '',
     source: '',
@@ -43,7 +43,7 @@ const projects = [
     name: 'Professional Art Printing data',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: ``,
+    image: '',
     technology: ['html', 'Bootstrap', 'Ruby on rails'],
     liveVersion: '',
     source: '',
@@ -53,7 +53,7 @@ const projects = [
     name: 'Professional Art Printing data',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    image: ``,
+    image: '',
     technology: ['html', 'Bootstrap', 'Ruby on rails'],
     liveVersion: '',
     source: '',
@@ -62,7 +62,7 @@ const projects = [
 
 const projectCards = document.querySelector('.works-card');
 
-projects.map(function (project) {
+projects.map((project) => {
   const div = document.createElement('div');
   div.className = 'work-card';
 
@@ -108,7 +108,7 @@ buttonOrangeBig.forEach((btn) => {
   });
 });
 
-modalClose.addEventListener('click',() => {
+modalClose.addEventListener('click', () => {
   modalContainer.classList.remove('active');
   modalOverlay.classList.remove('active');
 });
