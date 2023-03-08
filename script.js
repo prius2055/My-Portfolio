d2f67fe7791a460a6a5dd2449ec94c26a8d2fe6d
Prince <prius2055@gmail.com>
2023-03-08 12:22:36 +0100

    Applied linters recommendations

--- script.js
+++ script.js
@@ -60,12 +60,11 @@ const projects = [
   },
 ];
 
-const projectCards = document.querySelector(".works-card");
+const projectCards = document.querySelector('.works-card');
 
 projects.map((project) => {
-  console.log(project);
-  let div = document.createElement("div");
-  div.className = "work-card";
+  let div = document.createElement('div');
+  div.className = 'work-card';
 
   div.innerHTML = `<div class='work-card-details'><h2>${project.name}</h2><p>${project.description}</p><ul class='list list-grey'><li>${project.technology[0]}</li><li>${project.technology[1]}</li><li>${project.technology[2]}</li>
   </ul></div> <button class='btn-orange btn-orange-big'>See projects</button>`;
@@ -73,44 +72,43 @@ projects.map((project) => {
   projectCards.appendChild(div);
 });
 
-const mobileNav = document.querySelector(".mobile-nav");
-const hamburger = document.querySelector(".hamburger");
-const mobileNavLogo = document.querySelector(".logo");
-const mobileNavLink = document.querySelectorAll(".nav-link");
-const mainContainer = document.querySelector(".main-container");
-const bottomLine = document.querySelector(".bottom_line");
-const buttonOrangeBig = document.querySelectorAll(".btn-orange-big");
+const mobileNav = document.querySelector('.mobile-nav');
+const hamburger = document.querySelector('.hamburger');
+const mobileNavLogo = document.querySelector('.logo');
+const mobileNavLink = document.querySelectorAll('.nav-link');
+const mainContainer = document.querySelector('.main-container');
+const bottomLine = document.querySelector('.bottom_line');
+const buttonOrangeBig = document.querySelectorAll('.btn-orange-big');
 
-const modalContainer = document.querySelector(".modal-container");
-const modalOverlay = document.querySelector(".overlay");
-const modalClose = document.querySelector(".modal-close");
+const modalContainer = document.querySelector('.modal-container');
+const modalOverlay = document.querySelector('.overlay');
+const modalClose = document.querySelector('.modal-close');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  mobileNav.classList.toggle("active");
-  mobileNavLogo.classList.toggle("active");
-  mainContainer.classList.toggle("active");
-  bottomLine.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+  mobileNavLogo.classList.toggle('active');
+  mainContainer.classList.toggle('active');
+  bottomLine.classList.toggle('active');
 });
 
 mobileNavLink.forEach((nav) => {
-  nav.addEventListener("click", () => {
-    mobileNav.classList.remove("active");
-    hamburger.classList.remove("active");
+  nav.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+    hamburger.classList.remove('active');
   });
 });
 
 /* The modal */
 
 buttonOrangeBig.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    modalContainer.classList.add("active");
-    modalOverlay.classList.add("active");
-    console.log(buttonOrangeBig);
+  btn.addEventListener('click', () => {
+    modalContainer.classList.add('active');
+    modalOverlay.classList.add('active');
   });
 });
 
-modalClose.addEventListener("click", function () {
-  modalContainer.classList.remove("active");
-  modalOverlay.classList.remove("active");
+modalClose.addEventListener('click', function () {
+  modalContainer.classList.remove('active');
+  modalOverlay.classList.remove('active');
 });
