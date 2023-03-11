9bc96234baa3d132af6e5023632a4397615ea98f
Prince Nwuke <63577080+prius2055@users.noreply.github.com>
2023-03-11 10:24:46 +0100

    Added line breaks
    Added line breaks

--- script.js
+++ script.js
@@ -178,14 +178,12 @@ const form = document.querySelector('form');
 const email = document.querySelector('.input-email');
 const errorMsg = document.querySelector('.error');
 
-const emailPattern 
- = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const emailPattern = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 form.addEventListener('submit', (event) => {
   const isValid = email.value.length === 0 || emailPattern.test(email.value);
   if (!isValid) {
-    errorMsg.textContent 
-     = 'Your email address should all be in lower case. yourname@example.com';
+    errorMsg.textContent = 'Your email address should all be in lower case. yourname@example.com';
     errorMsg.className = 'error active';
     event.preventDefault();
   } else {
